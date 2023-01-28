38d8636695abdfe4abe68988b9429b60e4686268
anirudhadak2 <123630876+anirudhadak2@users.noreply.github.com>
2023-01-28 11:59:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,46 +1,60 @@
 <!Doctype html>
+
 <html>
 <head>
-            <title>Register Form</title>
+<title>form</title>
 </head>
-<body>
-<form>
-             <table>
-                       <tr>
-                           <td>
-                                     Name:
-                                    </td>
-                                     </td>
-                                                  <input type"text"placeholder"name"name="">
-                                                </td>
-                                            </tr>
-                                     <tr>
-                                      <td>
-                                       password:
-                                    </td>
-                                     <td>
-                                            <input type"passord"placeholder "password"name="">
-                 </td>
-              </tr>
-    <tr>
-<td>
-          gender :
-        </td>
-<td>
-                  <input type="radio"name"gender">male
-                   <input type"radio"name="gender">female
-       </td>
-     </tr>
-<tr>
-         
-        <td>
-                 Email :
-</td>
-   <td>
-            <input type ="mail" placeholder="Email" name="">
 
-<tr>
-</table>
+<body>
+<form.action="method="post"target="blank">
+<center>
+<caption align="top">
+<h2>Application form</h2>
+
+</cation>
+</center>
+<form>
+<fieldset><legend>personal information</legend>
+first name:
+<input type="text"value="harshal"><br>  <br>
+
+middle name:
+<input type="text"value="harshal"><br>  <br>
+
+last name:
+
+<input type="text"value="manoj">  
+
+</fieldset>
+<fieldset><legend>caption</caption>
+
+<input type="checkbox" name="1"value="ug">ug
+<input type="checkbox"name="2"value=""pg">pg   <br>
+<br>
+</fieldset>
+
+<fieldset><legend>gender</legend>
+
+gender:<br><br>
+
+<input type="radio"name="1"value=""male">
+male<br><br>
+<input type="radio"name="2"value="female">female<br><br>
+
+</fieldset>
+
+<fieldset><legend>country</legend>
+
+<select><br>
+
+<option>country</option>
+<option>india</option>
+<option>spain</option>
+<option>usa</option>
+<option>england</option></select
+</fieldset>
+
+<input type="submit" value="submit">
 </form>
 </body>
 </html>
