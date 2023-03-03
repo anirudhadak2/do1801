120539ef49923fa354d9b64abdbd91d427fb2945
anirudhadak2 <123630876+anirudhadak2@users.noreply.github.com>
2023-03-03 14:23:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,60 +1,93 @@
-<!Doctype html>
-
+<!DOCTYPE HTML>  
 <html>
 <head>
-<title>form</title>
+<style>
+.error {color: #FF0000;}
+</style>
 </head>
+<body>  
 
-<body>
-<form.action="method="post"target="blank">
-<center>
-<caption align="top">
-<h2>Application form</h2>
+<?php
+// define variables and set to empty values
+$nameErr = $emailErr = $genderErr = $websiteErr = "";
+$name = $email = $gender = $comment = $website = "";
 
-</cation>
-</center>
-<form>
-<fieldset><legend>personal information</legend>
-first name:
-<input type="text"value="harshal"><br>  <br>
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+  }
+    
+  if (empty($_POST["website"])) {
+    $website = "";
+  } else {
+    $website = test_input($_POST["website"]);
+  }
 
-middle name:
-<input type="text"value="harshal"><br>  <br>
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
 
-last name:
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
+}
 
-<input type="text"value="manoj">  
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
 
-</fieldset>
-<fieldset><legend>caption</caption>
-
-<input type="checkbox" name="1"value="ug">ug
-<input type="checkbox"name="2"value=""pg">pg   <br>
-<br>
-</fieldset>
-
-<fieldset><legend>gender</legend>
-
-gender:<br><br>
-
-<input type="radio"name="1"value=""male">
-male<br><br>
-<input type="radio"name="2"value="female">female<br><br>
-
-</fieldset>
-
-<fieldset><legend>country</legend>
-
-<select><br>
-
-<option>country</option>
-<option>india</option>
-<option>spain</option>
-<option>usa</option>
-<option>england</option></select
-</fieldset>
-
-<input type="submit" value="submit">
+<h2>PHP Form Validation </h2>
+<h3> hello Anirudha Here Please fill the from with valid details ! </h3>
+<p><span class="error">* required field</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br><br>
+  E-mail: <input type="text" name="email">
+  <span class="error">* <?php echo $emailErr;?></span>
+  <br><br>
+  Website: <input type="text" name="website">
+  <span class="error"><?php echo $websiteErr;?></span>
+  <br><br>
+  Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+  <br><br>
+  Gender:
+  <input type="radio" name="gender" value="female">Female
+  <input type="radio" name="gender" value="male">Male
+  <input type="radio" name="gender" value="other">Other
+  <span class="error">* <?php echo $genderErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
 </form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $website;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
+
 </body>
 </html>
